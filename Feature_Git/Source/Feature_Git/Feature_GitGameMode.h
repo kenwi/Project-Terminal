aa08c013a669f31a2bc84c1927b797cbfe33f1e0
kenwi <kenneth@wilhelmsen.nu>
2015-02-01 00:58:12 +0100

    Uploading basic code.
    No Assets included.

---
+++ Feature_Git/Source/Feature_Git/Feature_GitGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "Feature_GitGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FEATURE_GIT_API AFeature_GitGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
